c3d73fc623df0b41a01cb0a86ea6e5d550e7e63c
AlejandraO1 <alejandraortegax@gmail.com>
2023-07-28 15:13:03 -0400

    Added event listeners to each button and defined computerPlay function

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
+//Buttons
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
+
+rockButton.addEventListener("click", () => {
+  playRound("rock", computerPlay());
+});
+paperButton.addEventListener("click", () => {
+  playRound("paper", computerPlay());
+});
+scissorsButton.addEventListener("click", () => {
+  playRound("scissors", computerPlay());
+});
+
 //Computer Choices
 const gameArray = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
   return gameArray[~~(Math.random() * gameArray.length)];
+}
+
+function computerPlay() {
+  return getComputerChoice();
 }
 
 //Playing a round
@@ -26,7 +45,18 @@ function playRound(playerSelect, computerSelect) {
   }
 }
 
+//Playing a game
+function playGame() {
+  computerScore = 0;
+  playerScore = 0;
+  for (let i = 0; i < 5; i++) {
+    onUserInput();
+  }
+}
+
 let computerSelect = getComputerChoice();
 let playerSelect;
 let computerScore;
 let playerScore;
+
+playGame();
